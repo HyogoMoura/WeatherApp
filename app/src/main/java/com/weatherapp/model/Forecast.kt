292af746195c82b80144320dd37614a199d06309
HyogoMoura <diogofilipilsht@poli.br>
2025-08-28 20:31:30 -0300

    Prática 8 – Parte 3

--- app/src/main/java/com/weatherapp/model/Forecast.kt
+++ app/src/main/java/com/weatherapp/model/Forecast.kt
@@ -1,8 +1,6 @@
 package com.weatherapp.model
 
-class Forecast {
-    data class Forecast (
-        val date: String, val weather: String,
-        val tempMin: Double, val tempMax: Double, val imgUrl: String,
-    )
-}
+data class Forecast (
+    val date: String, val weather: String,
+    val tempMin: Double, val tempMax: Double, val imgUrl: String,
+)
